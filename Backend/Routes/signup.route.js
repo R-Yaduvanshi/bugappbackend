4d3fc180083885d2a64c9d6c1cd8791a3744ec39
RAJENDRA YADAV <100795038+R-Yaduvanshi@users.noreply.github.com>
2023-03-24 01:51:17 +0530

    Signupupdated

--- Backend/Routes/signup.route.js
+++ Backend/Routes/signup.route.js
@@ -6,17 +6,17 @@ const { UserModel } = require("../models/User.models");
 // Signup
 const signup = async (req, res) => {
   const { email, password } = req.body;
+  console.log(email, password);
   const user = await UserModel.findOne({ email });
   //   console.log(user);
 
   if (user) {
-    res.send({ Message: "User already exists" });
+    return res.status(400).json({ Message: "User already exists" });
   } else {
-    bcrypt.hash(password, 8, async (err, hash) => {
+    bcrypt.hash(password, 3, async (err, hash) => {
       if (err) {
         res.send({ Message: "Something went wrong" });
       }
-
       const new_user = new UserModel({
         email,
         password: hash,
@@ -27,6 +27,7 @@ const signup = async (req, res) => {
         res.send({ Message: "Signup successful" });
       } catch (err) {
         res.send({ Message: "Somthing Went Wrong" });
+        console.log("=>", err);
       }
     });
   }
